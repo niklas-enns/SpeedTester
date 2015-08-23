03d443b3ffc5bf89ffc51a7c705cfa5faa985560
niklasu <niklasu@users.noreply.github.com>
2015-08-23 23:38:19 +0200

    - Command line support by using JCommander library - Some cleanup with error checking - Target file size is now checked by looking at the 'Content-Length' field of a HEAD request to the target file. Not anymore by downloading the target file.

--- SpeedTester/src/downloadTestService/State.java
+++
@@ -1,8 +0,0 @@
-package downloadTestService;
-
-/**
- * Created by enzo on 19.02.2015.
- */
-public enum State {
-    IDLE,DOWNLOADING
-}
---
+++ SpeedTester/src/main/java/downloadTestService/Constants.java
@@ -0,0 +1,10 @@
+package downloadTestService;
+
+/**
+ * Created by enzo on 23.08.2015.
+ */
+public interface Constants {
+    public static final int KB = 1000;
+    public static final int MB = KB * 1000;
+    public static final int GB = MB * 1000;
+}
