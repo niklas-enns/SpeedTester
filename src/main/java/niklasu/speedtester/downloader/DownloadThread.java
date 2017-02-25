2be17eb25017e7810857d1b3361d71c4f037f783
niklasu <niklasu@users.noreply.github.com>
2017-02-25 16:07:12 +0100

    Non-UI mode works,

--- src/main/java/niklasu/speedtester/downloader/DownloadThread.java
+++ src/main/java/niklasu/speedtester/downloader/DownloadThread.java
@@ -1,32 +1,23 @@
 package niklasu.speedtester.downloader;
 
-import niklasu.speedtester.DownloadService;
-import niklasu.speedtester.downloader.DownloadFileSizeChecker;
 import niklasu.speedtester.interfaces.Constants;
 
-import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Logger;
 
-/**
- * Created by enzo on 06.11.2014.
- */
 public class DownloadThread extends Thread implements Constants {
     private static final Logger log = Logger.getLogger(DownloadFileSizeChecker.class.getName());
-    URL targetFile;
-    long downloadSize;
-    Date startOfDownload;
+    private URL targetFile;
+    private long downloadsizeInMB;
+    private Date startOfDownload;
 
-    public DownloadThread(long size, URL url) {
+    DownloadThread(long downloadsizeInMB, URL url) {
         targetFile = url;
-        downloadSize = size;
+        this.downloadsizeInMB = downloadsizeInMB;
     }
 
     public void run() {
@@ -34,26 +25,26 @@ public class DownloadThread extends Thread implements Constants {
         long startTime = startOfDownload.getTime();
         download();
         long runningTime = new Date().getTime() - startTime;
-        double resultSpeed = ((double) downloadSize / ((double) runningTime / 1000.)) / MB * 8.;
-        log.info("Download of " + downloadSize / MB + "MB completed, calculated Speed: " + String.format("%.2f", resultSpeed) + " MBit/s");
+        double resultSpeed = ((double) downloadsizeInMB / ((double) runningTime / 1000.)) * 8.;
+        log.info("Download of " + downloadsizeInMB + "MB completed, calculated Speed: " + String.format("%.2f", resultSpeed) + " MBit/s");
         publishResult(startOfDownload, resultSpeed);
-    }
-
-    private void publishResult(Date startOfDownload, double resultSpeed) {
-        //TODO via EventBus
-    }
+}
 
     private void download() {
         try {
             log.info("Starting download...");
             ReadableByteChannel rbc = Channels.newChannel(targetFile.openStream());
             FileOutputStream fos = new FileOutputStream("temp.dat");
-            fos.getChannel().transferFrom(rbc, 0, downloadSize);
+            fos.getChannel().transferFrom(rbc, 0, downloadsizeInMB * MB);
             rbc.close();
             //yes
         } catch (Exception e) {
             log.severe("Could not download file");
         }
+    }
+
+    private void publishResult(Date startOfDownload, double resultSpeed) {
+        //TODO via EventBus
     }
 
 }
---
+++ src/test/java/niklasu/speedtester/downloader/DownloadThreadTest.java
@@ -0,0 +1,20 @@
+package niklasu.speedtester.downloader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static niklasu.speedtester.interfaces.Constants.MB;
+
+class DownloadThreadTest {
+    private DownloadThread dlt;
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() throws MalformedURLException {
+        dlt = new DownloadThread(100*MB, new URL("http://ftp.halifax.rwth-aachen.de/opensuse/distribution/13.2/iso/openSUSE-13.2-DVD-i586.iso"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void run() {
+        dlt.run();
+    }
+
+}
