e77160f6d2fe05107a4620b0b01f1dca2535bbba
Niklas E <niklas.enns@gmail.com>
2018-01-05 21:36:29 +0100

    refactored DownloadThread

--- src/main/java/niklasu/speedtester/downloader/DownloadThread.java
+++ src/main/java/niklasu/speedtester/downloader/DownloadThread.java
@@ -6,7 +6,9 @@ import niklasu.speedtester.events.ResultEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
@@ -36,28 +38,29 @@ public class DownloadThread extends Thread {
     public void run() {
         Date startOfDownload = new Date();
         long startTime = startOfDownload.getTime();
-        download();
-        long runningTime = new Date().getTime() - startTime;
-        double resultSpeed = ((double) downloadsizeInMB / ((double) runningTime / 1000.)) * 8.;
-        logger.info("Download of " + downloadsizeInMB + "MB completed, calculated Speed: " + String.format("%.2f", resultSpeed) + " MBit/s");
-        publishResult(startOfDownload, resultSpeed);
-
+        try {
+            download();
+            long runningTime = new Date().getTime() - startTime;
+            double resultSpeed = ((double) downloadsizeInMB / ((double) runningTime / 1000.)) * 8.;
+            logger.info("Download of " + downloadsizeInMB + "MB completed, calculated Speed: " + String.format("%.2f", resultSpeed) + " MBit/s");
+            eventBus.post(new ResultEvent(startOfDownload, resultSpeed));
+        } catch (DownloadException e) {
+            logger.error("Download failed, because", e);
+            eventBus.post(new ResultEvent(startOfDownload, -1));
+        }
     }
 
-    private void download() {
+    private void download() throws DownloadException {
         try {
             logger.info(String.format("Starting download of %d MB from %s", downloadsizeInMB, targetFile.toString()));
             ReadableByteChannel rbc = Channels.newChannel(targetFile.openStream());
             FileOutputStream fos = new FileOutputStream("temp.dat");
             fos.getChannel().transferFrom(rbc, 0, downloadsizeInMB * MB);
             rbc.close();
-        } catch (Exception e) {
-            logger.error("Could not download file");
+        } catch (FileNotFoundException e) {
+            throw new DownloadException("", e);
+        } catch (IOException e) {
+            throw new DownloadException("", e);
         }
     }
-
-    private void publishResult(Date startOfDownload, double resultSpeed) {
-        eventBus.post(new ResultEvent(startOfDownload, resultSpeed));
-    }
-
 }
