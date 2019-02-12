b090faf18608d582b378cfbd318b3e51a32509dc
niklasu <niklasu@users.noreply.github.com>
2019-02-12 22:11:45 +0100

    WIP: migrating ParamValidator to Kotlin

--- src/test/java/niklasu/speedtester/config/KotlinParamValidatorTest.kt
+++ src/test/java/niklasu/speedtester/config/KotlinParamValidatorTest.kt
@@ -1,14 +1,11 @@
 package niklasu.speedtester.config
 
+import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.doThrow
 import com.nhaarman.mockito_kotlin.mock
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
 import java.io.IOException
-import java.net.MalformedURLException
 import java.net.URL
 
 internal class KotlinParamValidatorTest {
@@ -27,11 +24,10 @@ internal class KotlinParamValidatorTest {
     @Test
     @Throws(java.io.IOException::class)
     fun malformedURL() {
-        val fileSizeChecker = mock<FileSizeChecker>(FileSizeChecker::class.java)
-        `when`<Long>(fileSizeChecker.getFileSize(any<URL>())).thenThrow(MalformedURLException("url ist doof"))
-        val paramValidator = JavaParamValidator(fileSizeChecker)
+        val fileSizeChecker = mock<FileSizeChecker>{}
+        val paramValidator = KotlinParamValidator(fileSizeChecker)
 
-        val config = Config(100, 2, "http://fred.de")
+        val config = Config(100, 2, "httasdddasdp://fred.de")
 
         assertThrows(ValidationException::class.java) { paramValidator.validate(config) }.printStackTrace()
     }
@@ -39,9 +35,9 @@ internal class KotlinParamValidatorTest {
     @Test
     @Throws(java.io.IOException::class)
     fun fileIsTooSmall() {
-        val fileSizeChecker = mock<FileSizeChecker>(FileSizeChecker::class.java)
-        `when`<Long>(fileSizeChecker.getFileSize(any<URL>())).thenReturn(1L)
-        val paramValidator = JavaParamValidator(fileSizeChecker)
+        val url = URL("http://fred.de")
+        val fileSizeChecker = mock<FileSizeChecker>{on {getFileSize(url)} doReturn (1)}
+        val paramValidator = KotlinParamValidator(fileSizeChecker)
 
         val config = Config(100, 2, "http://fred.de")
 
@@ -51,9 +47,9 @@ internal class KotlinParamValidatorTest {
     @Test
     @Throws(java.io.IOException::class)
     fun intervalLessThanOne() {
-        val fileSizeChecker = mock<FileSizeChecker>(FileSizeChecker::class.java)
-        `when`<Long>(fileSizeChecker.getFileSize(any<URL>())).thenReturn(99999999999L)
-        val paramValidator = JavaParamValidator(fileSizeChecker)
+        val url = URL("http://fred.de")
+        val fileSizeChecker = mock<FileSizeChecker>{on {getFileSize(url)} doReturn (99999999999999)}
+        val paramValidator = KotlinParamValidator(fileSizeChecker)
 
         val config = Config(100, 0, "http://fred.de")
 
