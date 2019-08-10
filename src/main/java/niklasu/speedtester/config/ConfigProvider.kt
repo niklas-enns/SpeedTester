24bfa39627e9ad4a36f51fe0b0db3fe08c4ba9b5
niklasu <niklasu@users.noreply.github.com>
2019-08-10 16:25:22 +0200

    cleanup ConfigProvider

--- src/main/java/niklasu/speedtester/config/ConfigProvider.kt
+++ src/main/java/niklasu/speedtester/config/ConfigProvider.kt
@@ -2,14 +2,12 @@ package niklasu.speedtester.config
 
 import com.beust.jcommander.JCommander
 import com.beust.jcommander.Parameter
-import com.google.common.eventbus.EventBus
 import com.google.inject.Inject
 import com.google.inject.Singleton
-import niklasu.speedtester.events.ConfigChangedEvent
 
 @Singleton
 class ConfigProvider @Inject
-constructor(private val paramValidator: ParamValidator, private val eventBus: EventBus) {
+constructor(private val paramValidator: ParamValidator) {
     @Parameter(names = ["-size"], description = "Download size in MB")
     var size: Long = 50
         private set
@@ -19,27 +17,10 @@ constructor(private val paramValidator: ParamValidator, private val eventBus: Ev
     @Parameter(names = ["-url"], description = "file URL", required = true)
     var url = ""
         private set
-    @Parameter(names = ["-tray"], description = "Debug mode", arity = 1)
-    val isTray = true
 
     @Throws(ValidationException::class)
     fun setConfig(args: Array<String>) {
         JCommander(this, *args)
         paramValidator.validate(Config(size, interval, url))
-    }
-
-    @Throws(ValidationException::class)
-    fun setConfig(config: Config) {
-        paramValidator.validate(config)
-        this.size = config.fileSize
-        this.interval = config.interval
-        this.url = config.url
-        eventBus.post(ConfigChangedEvent())
-    }
-
-    fun reset() {
-        size = 50
-        interval = 1
-        eventBus.post(ConfigChangedEvent())
     }
 }
