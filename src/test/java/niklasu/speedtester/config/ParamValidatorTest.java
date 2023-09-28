7a4e9f7d3c9e4a5e1c672b1ff0a470eb2db76c7f
Niklas Enns (ext.) <niklas.enns@consol.de>
2023-09-28 22:02:16 +0200

    migrated ParamValidatorTest to java

---
+++ src/test/java/niklasu/speedtester/config/ParamValidatorTest.java
@@ -0,0 +1,51 @@
+package niklasu.speedtester.config;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+
+class ParamValidatorTest {
+
+    @Test
+    void NetworkError() throws IOException, FileSizeChecker.NoFileSizeException, ValidationException {
+        URL url = new URL("http://fred.de");
+        FileSizeChecker fileSizeChecker = mock(FileSizeChecker.class);
+        when(fileSizeChecker.getFileSize(any())).thenThrow(new IOException("kaputt"));
+
+        ParamValidator paramValidator = new ParamValidator(fileSizeChecker);
+
+        assertThrows(ValidationException.class, () -> paramValidator.validate(100L, 2, url.toString()));
+    }
+
+    @Test
+    void malformedURL() {
+        FileSizeChecker fileSizeChecker = mock(FileSizeChecker.class);
+        ParamValidator paramValidator = new ParamValidator(fileSizeChecker);
+
+        assertThrows(ValidationException.class, () -> paramValidator.validate(100L, 2, "httasdddasdp://fred.de"));
+
+    }
+
+    @Test
+    void fileIsTooSmall() throws IOException, FileSizeChecker.NoFileSizeException, ValidationException {
+        FileSizeChecker fileSizeChecker = mock(FileSizeChecker.class);
+        when(fileSizeChecker.getFileSize(any())).thenReturn(1L);
+        ParamValidator paramValidator = new ParamValidator(fileSizeChecker);
+        assertThrows(ValidationException.class, () -> paramValidator.validate(100L, 2, "http://fred.de"));
+    }
+
+    @Test
+    void intervalLessThanOne() throws FileSizeChecker.NoFileSizeException, ValidationException, IOException {
+        FileSizeChecker fileSizeChecker = mock(FileSizeChecker.class);
+        when(fileSizeChecker.getFileSize(any())).thenReturn(99999999999999L);
+        ParamValidator paramValidator = new ParamValidator(fileSizeChecker);
+
+        assertThrows(ValidationException.class, () -> paramValidator.validate(100L, 0, "http://fred.de"));
+    }
+}
